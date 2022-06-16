b27bff4cfe368f36f7f24669dbfed9266ff55abc
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-06-15 22:33:26 -0400

    Primer paso del programa.

---
+++ KataRange/Range.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataRange
+{
+    public class Range
+    {
+        string test;
+        public Range(string test)
+        {
+            this.test = test;
+        }
+
+        string FirstChar(string x)
+        {
+            return x.Substring(1, 1);
+
+        }
+        string SecondChar(string x)
+        {
+            return x.Substring(5, 1);
+
+        }
+        string FirstNumber(string x)
+        {
+            return x.Substring(2, 1);
+
+        }
+        string SecondNumber(string x)
+        {
+            return x.Substring(4, 1);
+
+        }
+
+        string GetAllPoint(string x)
+        {
+
+        }
+    }
+}
---
+++ TestProyect/UnitTest1.cs
@@ -0,0 +1,22 @@
+using KataRange;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+
+namespace TestProyect
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetAllPointsTest()
+        {
+            string x = "[2,6)";
+            Range range = new Range(x);
+            string expected = "{2,3,4,5}";
+
+            string actual = range.GetAllPoints(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
